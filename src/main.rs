90aada279758b7721a4c1b8160070f1c26a4ce68
Nertsal <sasha.kudasov04@gmail.com>
2022-09-02 17:47:31 +0300

    Setup geng

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use geng::prelude::*;
+
+struct State {
+    geng: Geng,
+}
+
+impl State {
+    pub fn new(geng: &Geng) -> Self {
+        Self { geng: geng.clone() }
+    }
+}
+
+impl geng::State for State {
+    fn draw(&mut self, framebuffer: &mut ugli::Framebuffer) {
+        ugli::clear(framebuffer, Some(Color::BLACK), None);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    logger::init().unwrap();
+    geng::setup_panic_handler();
+
+    let geng = Geng::new("Wave Function Collapse");
+
+    let state = State::new(&geng);
+
+    geng::run(&geng, state)
 }
