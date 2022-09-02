efe6cd6e5638a4dd2cd34477bef3a599bee18910
Vitaliy Alifanov <alifanov.vitalik@yandex.ru>
2022-09-02 18:12:39 +0300

    add main logic

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,40 @@ impl geng::State for State {
     }
 }
 
+
+pub struct Grid {
+    cells: Vec<Cell>,
+    width: usize,
+    length: usize,
+}
+
+impl Grid {
+    pub fn new(width: usize, length: usize) -> Self {
+        Self {
+            cells: vec![Cell { cell_type: None }; width * length],
+            width,
+            length,
+        }
+    }
+
+    pub fn get_cell(&self, x: usize, y: usize) {
+        todo!()
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Cell {
+    /// Option here means whether the cell has been generated
+    cell_type: Option<CellType>,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum CellType {
+    Vertical,
+    Horizontal,
+    Empty,
+}
+
 fn main() {
     logger::init().unwrap();
     geng::setup_panic_handler();
